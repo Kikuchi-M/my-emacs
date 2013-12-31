40281c29fa0cda42773776f6839d0719e3de499e
Motoki Kikuchi <bp_jda.mk@live.jp>
2014-01-01 04:35:36 +0900

    Update init.el.  - cofing system -> utf-8  - frame-font for linux  - package-archives  - input method for linux  - splitting window  - fix key bindings for replace commands  - support commands to paredit  - el utilities  - new packages -> direx, magit, google-c-style, auto-complete

--- init.el
+++ init.el
@@ -1,11 +1,24 @@
+(setq default-buffer-file-coding-system 'utf-8)
+
 (when (eq system-type 'windows-nt) (set-frame-font "Consolas"))
+(when (eq system-type 'gnu/linux)
+  (set-frame-font
+   "-unknown-DejaVu Sans Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1"))
 
 (defconst emacs-add-dir "~/emacs-add")
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(when (eq system-type 'gnu/linux)
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/emacs-mozc")
+  (require 'mozc)
+  (setq default-input-method "japanese-mozc"))
 
 ;; ----- unbinded keys -----
 (global-unset-key (kbd "C-/"))
 (global-unset-key (kbd "C-z"))
-(global-unset-key (kbd "C-\\"))
 (setq inhibit-startup-message t)
 
 ;; ----- frames, windows -----
@@ -26,62 +39,144 @@
 (global-set-key (kbd "M-C-<down>") 'shrink-window)
 (global-set-key (kbd "M-C-<up>") 'enlarge-window)
 
-;; ----- display -----
+;; ----- display, faces -----
 (setq-default truncate-lines t)
 (defun truncate-lines-off () (setq truncate-lines nil))
 
 (setq-default column-number-mode t)
 
-(setq-default linum-mode t)
+(global-linum-mode t)
 (defun linum-mode-off () (linum-mode 0))
 
-;; ----- shell, interpreter -----
+;; turncate lines off
+(dolist (hooks (list
+                'comint-output-filter-functions
+                'ielm-mode-hook
+                'eshell-mode-hook
+                'messages-buffer-mode-hook))
+  (add-hook hooks (lambda (&optional opt)
+                    (truncate-lines-off))))
+
+;; line number mode off
 (dolist (hooks (list
                 'comint-output-filter-functions
                 'ielm-mode-hook
                 'eshell-mode-hook))
   (add-hook hooks (lambda (&optional opt)
-                    (truncate-lines-off)
                     (linum-mode-off))))
 
+(set-face-attribute 'region nil :background "SteelBlue1")
+
+(global-set-key
+ (kbd "C-x C-3")
+ (lambda (&optional n)
+   (interactive "P")
+   (let ((n1 (if (null n) 2 (- n 1))))
+     (loop repeat n1
+           do (progn
+                (split-window-horizontally)
+                (balance-windows-area))))))
+
+;; ----- interpreter -----
 (global-set-key (kbd "C-/ C-i") 'ielm)
 
-;; ----- file coding system -----
-(add-hook 'find-file-hook
-          (lambda ()
-            (when (and (stringp buffer-file-name)
-                       (string-match "\\.el\\'" buffer-file-name))
-              (set-buffer-file-coding-system 'undecided-unix)
-              (set-buffer-modified-p nil))))
+;; ----- project, directories -----
+(add-to-list 'load-path (concat emacs-add-dir "/direx-el"))
+(require 'direx)
 
+(add-to-list 'load-path (concat emacs-add-dir "/magit"))
+(require 'magit)
 
-;; ----- edit mode -----
-(add-to-list 'load-path (concat emacs-add-dir "/editors"))
+;; ----- edit mode, editing support -----
+;;  https://code.google.com/p/google-styleguide/
+(add-to-list 'load-path (concat emacs-add-dir "/google-style-el"))
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook
+          (lambda (&optional opt)
+            (google-set-c-style)))
+
+;; auto-complete - https://github.com/auto-complete/auto-complete
+;; auto-complete dependes on poup-el - https://github.com/auto-complete/popup-el
+(add-to-list 'load-path (concat emacs-add-dir "/popup-el"))
+(add-to-list 'load-path (concat emacs-add-dir "/auto-complete"))
+(require 'auto-complete)
+(global-auto-complete-mode 1)
+
+;; paredit - http://mumble.net/~campbell/emacs/
+(add-to-list 'load-path (concat emacs-add-dir "/paredit"))
 
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-;; unbinded keys M-<down>, M-<up>
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+;; unbinded keys M-<down>, M-<up>, C-M-f, C-M-b
+(dolist (hooks (list
+                 'emacs-lisp-mode-hook
+                 'eval-expression-minibuffer-setup-hook
+                 'ielm-mode-hook
+                 'lisp-mode-hook
+                 'lisp-interaction-mode-hook
+                 'scheme-mode-hook
+                 'c-mode-hook
+                 'c++-mode-hook))
+  (add-hook hooks 'enable-paredit-mode))
 
+(add-to-list 'load-path (concat emacs-add-dir "/qml-simple-mode"))
 (autoload 'qml-simple-mode "qml-simple-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.qml\\'" . qml-simple-mode))
 
-;; ----- tab, indent -----
+;; tab, indent
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
-;; ----- editing -----
+;; key bindings
 (global-set-key (kbd "C-z") 'undo)
-(global-set-key (kbd "C-/ C-s") 'replace-string)
-(global-set-key (kbd "C-/ C-r") 'replace-regexp)
-(global-set-key (kbd "C-/ C-q") 'query-replace)
-(global-set-key (kbd "C-/ C-x") 'query-replace-regexp)
+(global-set-key (kbd "C-; C-s") 'replace-string)
+(global-set-key (kbd "C-; C-r") 'replace-regexp)
+(global-set-key (kbd "C-; C-q") 'query-replace)
+(global-set-key (kbd "C-; C-x") 'query-replace-regexp)
+
+(defun replace-regexp-all (regexp to-string)
+  (let ((l (line-number-at-pos)))
+    (goto-char 0)
+    (replace-regexp regexp to-string)
+    (goto-line l)))
+
+;; Remove spaces in back of each line.
+(global-set-key
+ (kbd "C-; C-<SPC>")
+ (lambda (&optional opt)
+   (interactive)
+   (replace-regexp-all " +$" "")))
+
+;; Remove spaces which are input automatically
+;; in front of parentheses by paredit.
+(global-set-key
+ (kbd "C-; C-8")
+ (lambda (&optional opt)
+   (interactive)
+   (let* ((m (buffer-mode (current-buffer))))
+     (if (or (eq m 'qml-simple-mode) (c-major-mode-is m))
+         (replace-regexp-all "\\([^=]\\) +\\((\\)" "\\1\\2")
+       (display-message-or-buffer "The buffer mode is not compatible.")))))
 
 ;; ----- desktop -----
 (desktop-save-mode t)
 (global-set-key (kbd "C-/ C-d") 'desktop-change-dir)
 (add-hook 'desktop-after-read-hook (lambda () (setq search-ring nil)))
+
+
+;; ----- el utility -----
+(defun buffer-mode (buffer-or-string)
+  "Get major mode of paticular buffer."
+  (with-current-buffer buffer-or-string major-mode))
+
+(defun eval-sexp-forwardn (&optional n)
+  (interactive "P")
+  (if (not (eq (buffer-mode (current-buffer)) 'emacs-lisp-mode))
+      (display-message-or-buffer "The buffer is not elisp.")
+    (let ((n1 (if (null n) 1 n)))
+      (loop repeat n1
+            do (let ((b (point))
+                     (e (progn (forward-sexp) (point))))
+                 (eval-region b e))))))
+
+(global-set-key (kbd "C-; C-e") 'eval-sexp-forwardn)
+(global-set-key (kbd "C-; e") 'eval-sexp-forwardn)
