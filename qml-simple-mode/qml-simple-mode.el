b2b2f1583edf0e6348033db911aabf5de054b2cc
Motoki Kikuchi <bp_jda.mk@live.jp>
2014-01-02 04:28:02 +0900

    Fix syntax highlight rules.

--- qml-simple-mode/qml-simple-mode.el
+++ qml-simple-mode/qml-simple-mode.el
@@ -1,26 +1,28 @@
 ;; Simple QML Mode
 
+(defvar qml-simple-mode-hook nil)
+
 (let ((qml-highlight-blue "MediumBlue")      ;#0000cd
       (qml-highlight-orchid "DarkOrchid")    ;#9932cc
       (qml-highlight-olive "OliveDrab")      ;#6b8e23
-      (qml-highlight-red "red4")             ;#8b0000  
+      (qml-highlight-red "red4")             ;#8b0000
       )
-  
+
   (defface qml-specifier-face
     `((t :foreground ,qml-highlight-blue))
     "Face for element specifier.")
   (defvar qml-specifier-face 'qml-specifier-face)
-  
+
   (defface qml-preprocessor-face
     `((t :foreground ,qml-highlight-orchid))
     "Face for preprocessor.")
   (defvar qml-preprocessor-face 'qml-preprocessor-face)
-  
+
   (defface qml-package-face
     `((t :foreground ,qml-highlight-olive))
     "Face for package name.")
   (defvar qml-package-face 'qml-package-face)
-  
+
   (defface qml-package-version-face
     `((t :foreground ,qml-highlight-red))
     "Face for package version.")
@@ -36,66 +38,59 @@
     qml-st)
   "Syntax table for qml-simple-mode.")
 
-(defvar qml-specifier-list
-  '("Item" "Rectangle"))
-
 (defvar qml-font-lock-keywords
+  (let* ((separator "\\|")
+         (qml-directive-kwd
+          (mapconcat 'identity '("import" "using") separator))
+         )
+    (list
+     ;; preprocessor
+     (list (concat "^[ \t]*\\(" qml-directive-kwd "\\)[ \t]+"
+                   "\\("
+                   "\\(\\(\\([a-zA-Z][a-zA-Z0-9]*\\)\\.?\\)*\\([a-zA-Z][a-zA-Z0-9]*\\)\\)" ;; packages
+                   "[ \t]+"
+                   "\\(\\(\\([0-9]+\\)\\.?\\)*\\([0-9]+\\)\\)" ;; version
+                   "\\|"
+                   "\\(\"[^ ]+\"\\)" ;; directory
+                   "\\)"
+                   "[ \t]*;?$")
+           '(1 qml-preprocessor-face nil t)
+           '(3 qml-package-face nil t)
+           '(7 qml-package-version-face nil t))
 
-;;  "Highlighting expression for qml-simple-mode."
-  )
+     ;; specifiers
+     (list (concat "\\(^\\|\\*/\\|:[ \t]*\\)"
+                   "[ \t]*"
+                   "\\([A-Z][a-zA-Z0-9]*\\)[ \t]*\\({\\|$\\)")
+           2 qml-specifier-face)
 
-(defun init-qml-font-lock-keywords ()
-  (setq qml-font-lock-keywords 
-        (let* ((separator "\\|")
-               (qml-directive-kwd
-                (mapconcat 'identity '("import" "using") separator))
-               (qml-specifier-kwd
-                (mapconcat 'identity qml-specifier-list separator)))
-          (list
-           (list (concat "^[ \t]*\\(" qml-directive-kwd "\\)[ \t]+" 
-                         "\\(\\(\\([a-zA-Z][a-zA-Z0-9]*\\)\\.?\\)*\\([a-zA-Z][a-zA-Z0-9]*\\)\\)"
-                         "[ \t]+"
-                         "\\(\\(\\([0-9]+\\)\\.?\\)*\\([0-9]+\\)\\)" 
-                         "[ \t]*;?$")
-                 '(1 qml-preprocessor-face nil t)
-                 '(2 qml-package-face nil t)
-                 '(6 qml-package-version-face nil t)
+     ;; props
+     (list (concat "\\(^[ \t]*\\|;[ \t]*\\|{[ \t]*\\)"
+                   "\\(\\([a-zA-Z][a-zA-Z0-9]*\\)\\.\\)?\\([a-z][a-zA-Z0-9]*\\)"
+                   "[ \t]*[:{]")
+           '(3 font-lock-variable-name-face nil t)
+           '(4 font-lock-variable-name-face nil t))
+     )))
 
-                 )
-           (list (concat "^[ \t]*\\(" qml-directive-kwd "\\)[ \t]+" "\\(\".+\"\\)")
-                 '(1 qml-preprocessor-face nil t))
-
-           ;; specifiers
-           ;;(list "\\(^\\|\\*/\\)[ \t]*\\([a-zA-Z][a-zA-Z0-9]*\\)[ \t]*\\({\\|$\\)" 2 qml-specifier-face)
-           (cons (concat "\\<\\(" qml-specifier-kwd "\\)\\>") qml-specifier-face)
-
-           ;; props
-           (list (concat "\\(^[ \t]*\\|;[ \t]*\\|{[ \t]*\\)"
-           "\\([a-zA-Z][a-zA-Z0-9]*\\)\\(\\.\\([a-zA-Z][a-zA-Z0-9]*\\)\\)?[ \t]*:") 
-                 '(2 font-lock-variable-name-face nil t)
-                 '(3 font-lock-variable-name-face nil t))
-           ;;'("/\\*.*\\*/\\|//.*" (0 font-lock-comment-face t t))
-           ;; keywords for multi-line block comment is later...
-           ))))
-(init-qml-font-lock-keywords)
+(defvar qml-indent-offset 2)
 
 (defun qml-indent-line ()
   "Indent current line according to QML indentation rule."
   (interactive)
-  (display-message-or-buffer "*** qml-simple-mode *** not implemented.")
+  (display-message-or-buffer "*** qml-simple-mode *** not implemented qml-indent-line function.")
   )
 
 (define-derived-mode qml-simple-mode fundamental-mode "QML"
   "Mejor mode for Qt declarative UI (simple mode)"
   (interactive)
   (kill-all-local-variables)
-  :syntax-table qml-simple-syntax-table
+  (set-syntax-table qml-simple-syntax-table)
   (set (make-local-variable 'font-lock-defaults) '(qml-font-lock-keywords))
-  ;;  (set (make-local-variable 'indent-line-function) 'qml-indent-line)
+  (set (make-local-variable 'tab-width) qml-indent-offset)
+  ;;(set (make-local-variable 'indent-line-function) 'qml-indent-line)
   (setq major-mode 'qml-simple-mode)
   (setq mode-name "QML")
+  (run-hooks 'qml-simple-mode-hook)
   )
-
-
 
 (provide 'qml-simple-mode)
