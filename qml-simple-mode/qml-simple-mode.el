26f79546c531043059f0cef7c46067d83f5eba20
Motoki Kikuchi <bp_jda.mk@live.jp>
2014-01-04 08:37:39 +0900

    Implement indent-line-function of qml-simple-mode.

--- qml-simple-mode/qml-simple-mode.el
+++ qml-simple-mode/qml-simple-mode.el
@@ -72,13 +72,67 @@
            '(4 font-lock-variable-name-face nil t))
      )))
 
+(defun qml-beginning-of-block-internal (prev cur)
+  "This function used in `qml-beginning-of-block' function.
+If want the position of beginning of block, use it."
+  (if (not prev) nil
+    (let* ((prev1
+            (save-excursion
+              (goto-char prev)
+              (if (search-backward-regexp
+                   "\\(^[ \t]*\\|\\*/[ \t]*\\)[a-zA-Z\.]*:?[ \t]*\\({\\)[ \t]*$"
+                   nil t)
+                  (match-beginning 2)
+                nil)))
+           (end-of-prev1 (if (not prev1) nil
+                   (condition-case nil
+                       (save-excursion (goto-char prev1)
+                                       (forward-sexp)
+                                       (point))
+                     (error nil)))))
+      (if (not prev1) nil
+        (if (or (not end-of-prev1)
+                (and (> cur prev1)
+                     (< (save-excursion
+                          (goto-char cur) (end-of-line) (point))
+                        end-of-prev1)))
+            prev1
+          (qml-beginning-of-block-internal prev1 cur))))))
+
+(defun qml-beginning-of-block (&optional cur)
+  "Get position of beginning of block.
+Return the position of beginning parenthesis \"{\" if found it, otherwise return nil.
+If CUR is nil, try find beginning of block from current position.
+In the line CUR is in, an item is declared single line (like that
+\"Item { id:item1 ... }\" is in a line.), return the position
+of beginning of \"item1\"'s parent block."
+  (let* ((p (if (numberp cur) cur (point)))
+         (qbob (qml-beginning-of-block-internal
+                (save-excursion (goto-char p)
+                                (beginning-of-line)
+                                (point))
+                p)))
+    (if (not qbob)
+        (display-message-or-buffer "Not found beginning of block"))
+    qbob))
+
 (defvar qml-indent-offset 2)
 
 (defun qml-indent-line ()
   "Indent current line according to QML indentation rule."
   (interactive)
-  (display-message-or-buffer "*** qml-simple-mode *** not implemented qml-indent-line function.")
-  )
+  (let* ((qbob (qml-beginning-of-block (point)))
+         (pi (if (not qbob) nil
+               (save-excursion
+                 (goto-char qbob)
+                 (current-indentation)))))
+    (when pi
+      (let ((ni (+ pi qml-indent-offset))
+            (ci (current-indentation)))
+        (if (not (eq ni ci)) (save-excursion (indent-line-to ni)))
+        (let ((lbp (line-beginning-position)))
+          (if (< (- (point) lbp) ni) (goto-char (+ lbp ni))))))
+    ))
 
 (define-derived-mode qml-simple-mode fundamental-mode "QML"
   "Mejor mode for Qt declarative UI (simple mode)"
@@ -87,7 +141,7 @@
   (set-syntax-table qml-simple-syntax-table)
   (set (make-local-variable 'font-lock-defaults) '(qml-font-lock-keywords))
   (set (make-local-variable 'tab-width) qml-indent-offset)
-  ;;(set (make-local-variable 'indent-line-function) 'qml-indent-line)
+  (set (make-local-variable 'indent-line-function) 'qml-indent-line)
   (setq major-mode 'qml-simple-mode)
   (setq mode-name "QML")
   (run-hooks 'qml-simple-mode-hook)
